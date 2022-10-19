6df6dd1e6908690c6346ea569b0cb6a5d05d86c4
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-19 18:24:52 -0300

    Definindo o Tema da Aplicação

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Groups } from '@screens/Groups';
+import theme from '@theme/index';
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
