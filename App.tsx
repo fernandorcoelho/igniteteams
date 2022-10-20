88576bfd212df1370e35b722d61770048c46275d
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-19 21:55:00 -0300

    Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
+import { ActivityIndicator } from 'react-native';
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
 import theme from '@theme/index';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
