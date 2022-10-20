4bfe015f05a9ef97602114295be996ad3e79c156
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-20 15:12:32 -0300

    Criando Interface NewGroup

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   Roboto_700Bold
 } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import theme from '@theme/index';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
@@ -15,7 +16,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
