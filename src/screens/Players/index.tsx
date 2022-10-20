ecbf80c2f0ef1cf20cf3c791f5b4eb17be229505
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-20 16:22:13 -0300

    Input com ícone

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,7 @@
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
 
 import * as S from './styles';
 
@@ -12,7 +13,11 @@ export function Players() {
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
       />
-      <ButtonIcon icon="home" />
+
+      <S.Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </S.Form>
     </S.Container>
   );
 }
