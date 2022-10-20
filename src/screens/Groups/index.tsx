e38c073461f4ca69ee39c503b40b5de571fd4136
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-20 11:31:29 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -5,11 +8,18 @@ import { Highlight } from '@components/Highlight';
 import * as S from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = React.useState<string[]>(['Galera da Rocket']);
+
   return (
     <S.Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com sua turma" />
-      <GroupCard title="Galera do Ignite" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </S.Container>
   );
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -17,8 +17,8 @@ export default {
     GRAY_100: '#E1E1E6'
   },
   FONT_FAMILY: {
-    REGULAR: 'ROBOTO_400Regular',
-    BOLD: 'ROBOTO_700Bold'
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold'
   },
   FONT_SIZE: {
     SM: 14,
