5cd90ab16a794bc284c70e30ebe304a263fee01e
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-21 09:26:35 -0300

    Finalizando a lista de Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 
+import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { ListEmpty } from '@components/ListEmpty';
+import { PlayerCard } from '@components/PlayerCard';
 
 import * as S from './styles';
 
 export function Players() {
   const [team, setTeam] = React.useState('Time A');
-  const [players, setPlayers] = React.useState([]);
+  const [players, setPlayers] = React.useState([
+    'Fernando',
+    'Maju',
+    'Lucas',
+    'Alessandro',
+    'Milton',
+    'Davi',
+    'Plant',
+    'Macumba'
+  ]);
 
   return (
     <S.Container>
@@ -41,6 +53,29 @@ export function Players() {
         />
         <S.NumberOfPlayers>{players.length}</S.NumberOfPlayers>
       </S.HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => {
+              return;
+            }}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </S.Container>
   );
 }
