0fb82edf568453723ab20771db490a63ca447f35
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2022-10-21 09:37:36 -0300

    Revisão de Layout

--- App.tsx
+++ App.tsx
@@ -5,11 +5,11 @@ import {
   Roboto_700Bold
 } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
-import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+// import { NewGroup } from '@screens/NewGroup';
+// import { Players } from '@screens/Players';
 import theme from '@theme/index';
 import { StatusBar } from 'expo-status-bar';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
